aa1e3ef9f65f52447b007f3ec73a1db2bc3b272d
janett22 <42725888+janett22@users.noreply.github.com>
2021-04-08 02:54:06 -0300

    Inmobiliaria
    ABM Propietario y inquilino

---
+++ Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaJanett.Models
+{
+    public class Persona
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+
+    }
+}
